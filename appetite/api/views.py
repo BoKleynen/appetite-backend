c0c61b076d50a5eba897c9814a5fc215a4acd922
Frederik <baetens.fr@gmail.com>
2019-10-26 16:58:00 +0200

    add the rest of the api

--- appetite/api/views.py
+++ appetite/api/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from appetite.api.models import MenuItem, OrderItem, Order, Category, Venue
-from appetite.api.serializers import UserSerializer, GroupSerializer, MenuItemSerializer
+from appetite.api.serializers import UserSerializer, GroupSerializer, MenuItemSerializer, OrderSerializer, OrderItemSerializer, VenueSerializer, CategorySerializer
 
 
 # Create your views here.
@@ -33,10 +33,40 @@ class GroupViewSet(viewsets.ModelViewSet):
 
 class MenuItemViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows groups to be viewed or edited.
+    API endpoint that allows MenuItems to be viewed or edited.
     """
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
 
+class OrderViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Orders to be viewed or edited.
+    """
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class VenueViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Venues to be viewed or edited.
+    """
+    queryset = Venue.objects.all()
+    serializer_class = VenueSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Categorys to be viewed or edited.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows OrderItems to be viewed or edited.
+    """
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
 # class
