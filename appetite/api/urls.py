29fed800f606767bc1de6bd56be13b4f68199afe
Frederik <baetens.fr@gmail.com>
2019-10-26 07:21:12 +0200

    api for users

--- appetite/api/urls.py
+++ appetite/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
+from appetite.api import views
 
 urlpatterns = [
     # path('users/', include('users.urls')),
     path('rest-auth/', include('rest_auth.urls')),
+    path('hello_world', views.hello_world),
 ]
