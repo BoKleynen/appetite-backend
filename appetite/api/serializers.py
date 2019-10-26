70fdc48fc68bf7da95755f7a4ab1af39c64d534a
Bo <kleynenbo@gmail.com>
2019-10-26 21:09:56 +0300

    Return nested objects instead of links

--- appetite/api/serializers.py
+++ appetite/api/serializers.py
@@ -3,31 +3,31 @@ from rest_framework import serializers
 from appetite.api.models import MenuItem, Order, Venue, Category, OrderItem
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
 
 
-class MenuItemSerializer(serializers.HyperlinkedModelSerializer):
+class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = ['url', 'name', 'price', 'extra_info', 'category', 'venue']
 
 
-class OrderSerializer(serializers.HyperlinkedModelSerializer):
+class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ['url', 'status', 'customer', 'table', 'items', 'created_at']
 
 
-class VenueSerializer(serializers.HyperlinkedModelSerializer):
+class VenueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Venue
         fields = ['url', 'name', 'info']
@@ -39,7 +39,7 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['url', 'name']
 
 
-class OrderItemSerializer(serializers.HyperlinkedModelSerializer):
+class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = ['url', 'amount', 'item', 'order']
